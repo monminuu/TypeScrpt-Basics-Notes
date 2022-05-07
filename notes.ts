3f9693af16e436d181a3acb91919519b262d3da1
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-05-07 21:44:06 +0530

    Notes Folder > Misconception 1

--- notes.ts
+++ notes.ts
@@ -2,3 +2,13 @@
 
 // Typescript  enforce types only at compile time & not runtime
 
+// Explanation:
+// Suppose there's function called 'getName' in function.ts folder
+// Now we have to export this function to js-function.js folder
+// by simply importing 'getName' i.e [import getName from './function.ts'] will not work.
+
+
+// Q. how to import typescript function into javascript folder?
+// A. run command: npx tsc function.ts
+//      it will give us function.js.
+//      ts will compile itself into js force.
