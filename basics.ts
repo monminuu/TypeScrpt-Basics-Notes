b26124f7133c90aac403ecb2141ce2a0e4014fa2
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-05-07 13:17:10 +0530

    Specifying Types >> Typing Array > interface & Record utility type

--- basics.ts
+++ basics.ts
@@ -16,7 +16,33 @@ let myRegex: RegExp = /foo/;
 const names: string[] = userName.split(" ");
 const myValues: Array<number> = [1, 2, 3, 4];
 
+//Typing Object---------------------
 
+const myPerson: {
+    first: string,
+    last: string
+} = {
+    first: "Monika",
+    last: "Singh"
+}
+// or we can define the type at once, so that when we need to change it, it will change everywhere.
+
+interface Person {
+    first: string,
+    last: string
+}
+
+const personName: Person = {
+    first: "Jim",
+    last: "Carry"
+}
+
+
+const ids: Record<number, string> = {
+    10: "a",
+    20: "b"
+}
+ids[30] = "c";
 
 
 
