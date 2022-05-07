a9dd47fcb7394deee2ed2b881a074774d9062fac
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-05-07 12:42:28 +0530

    Specifying Types

---
+++ basics.ts
@@ -0,0 +1,12 @@
+//SPECIFYING TYPES-------------------------------------------
+
+let userName: string = "Monika";
+let hasLoggedIn: boolean = true;
+
+userName += " Singh";
+
+console.log(userName);
+
+let myNumber: number = 10;
+let myRegex: RegExp = /foo/;
+
