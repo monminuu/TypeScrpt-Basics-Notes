c179d10650a8f7ee8f78ce36c0b8332796f4a0fa
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-05-08 11:04:53 +0530

    Returning Function

--- funcs-and-funcs.ts
+++ funcs-and-funcs.ts
@@ -23,3 +23,13 @@ console.log(arrayMutate([1, 2, 3], (v) => v * 10));
 //exporting MutationFunction to another function
 
 const newMutateFunc: MutationFunction = (v: number) => v * 100;
+
+//Returning Function----------------
+
+export function createAdder(num: number) {
+    return (val: number) => num + val;
+}
+
+const addOne = createAdder(1);
+
+console.log(addOne(65));
