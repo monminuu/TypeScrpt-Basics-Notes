6bd5e972d971d61f3648b339b4a072c9dbc18b41
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-05-08 13:46:24 +0530

    Function Overloading >> added if statement to parseCoordinate()

--- parseCoordinate.ts
+++ parseCoordinate.ts
@@ -24,12 +24,29 @@ function parseCoordinateFromNumbers(x: number, y: number): Coordinate {
 function parseCoordinate(obj: Coordinate): Coordinate;
 function parseCoordinate(x: number, y: number): Coordinate;
 
+
 // Why a question mark after arg2? "?" is used to make arg2 an optional argument.
-// Why arg2 is optional here? coz in the above function ( obj) is a single params & (x, y) are two params, if arg2
+// Why arg2 is optional here? coz in the above function ( obj) is a single params & (x, y) are two params, so ? implies that we can you either one or both params
 function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
     let coord: Coordinate = {
         x: 0,
         y:0,
     };
+
+    if (typeof arg1 === "object"){
+        coord = {
+            ...(arg1 as Coordinate),
+        };
+    } else {
+        coord = {
+            x: arg1 as number,
+            y: arg2 as number
+        };
+    }
+
     return coord;
 }
+
+console.log(parseCoordinate(80, 70));
+
+
