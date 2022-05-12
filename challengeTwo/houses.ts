a42db1dfb74db07c6e8fc3b0693e3b0b39ee3932
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-05-12 15:54:56 +0530

    Challenge yet to be completed

---
+++ challengeTwo/houses.ts
@@ -0,0 +1,6 @@
+export default
+[
+  { "name": "Atreides", "planets": "Calladan" },
+  { "name": "Corrino", "planets": ["Kaitan", "Salusa Secundus"] },
+  { "name": "Harkonnen", "planets": ["Giedi Prime", "Arrakis"] }
+]
